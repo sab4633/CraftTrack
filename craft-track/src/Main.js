72034b978c897a1b56fb698cd6f7f0764b05a09a
Sofia Bzhilyanskaya <sab4633@rit.edu>
2021-03-26 17:45:23 -0400

    Constantly updating the state variabales with database information

--- craft-track/src/Main.js
+++ craft-track/src/Main.js
@@ -29,53 +29,99 @@ export default class Main extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
 }
 
-handleChange(){
-
-}
-
-handleSubmit(){
-
-}
-
-renderData(){
-    const dataCopy = this.state.data;
-    let renderedData = [];
-    const skillKeys = Object.keys(dataCopy);
-    const FORMAT = ": "
-    for(const skill in skillKeys){
-        // alert([skillKeys[skill]]);
-        const currentSkill = skillKeys[skill];
-        renderedData.push(
-            
-            <p>{[currentSkill]}</p>
-        )
-        const projKeys = Object.keys(dataCopy[currentSkill])
-        for(const project in projKeys){
-            const currentProject = projKeys[project];
-            renderedData.push(
-            <p>
-                {currentProject}
-                {FORMAT}
-                {dataCopy[currentSkill][currentProject]}
-            </p>
-            )
-        }
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
-    return renderedData;
+    handleSubmit(e) {
+        e.preventDefault();
+        const skillsRef = firebase.database().ref('skills');
+        const skill = {
+            name: this.state.skill,
+            projects: {},
+        }
+        skillsRef.push(skill);
+        this.setState({
+            skill: '',
+            project: '',
+        });
+    }
 
-}
+    componentDidMount() {
+        const skillsRef = firebase.database().ref('skills');
+        skillsRef.on('value', (snapshot) => {
+            let data = snapshot.val();
+            let newState = [];
+            for (let skill in data) {
+                
+                const name = data[skill].name;
+                const projects = data[skill].projects;
+                if(projects){
+                    newState[name] = projects;
+                }else{
+                    newState[name] = {};
+                }
+                
+            //     newState.push({
+            //         id: skill,
+            //         title: data[skill].title,
+            //         user: data[skill].user
+            //     });
+            }
+            this.setState({
+                data: newState,
+            });
+        });
+    }
+
+    renderData(){
+        const dataCopy = this.state.data;
+        let renderedData = [];
+        const skillKeys = Object.keys(dataCopy);
+        const FORMAT = ": "
+        for(const skill in skillKeys){
+            // alert([skillKeys[skill]]);
+            const currentSkill = skillKeys[skill];
+            renderedData.push(
+                
+                <p>{[currentSkill]}</p>
+            )
+            const projKeys = Object.keys(dataCopy[currentSkill])
+            for(const project in projKeys){
+                const currentProject = projKeys[project];
+                renderedData.push(
+                <p>
+                    {currentProject}
+                    {FORMAT}
+                    {dataCopy[currentSkill][currentProject]}
+                </p>
+                )
+            }
+        }
+        return renderedData;
+
+    }
 
     render(){
         return(
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="skill" placeholder="new skill" onChange={this.handleChange} value={this.state.skill} />
-                    <input type="text" name="project" placeholder="new project" onChange={this.handleChange} value={this.state.project} />
-                    <button>Add Skill</button>
-                </form>
+            <div className="MainContainer">
+                <header>
+                    <div className="wrapper">
+                        <h1>CraftTrack</h1>        
+                    </div>
+                </header>
+                <div className='container'>
+                    <section className='add-item'></section>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" name="skill" placeholder="new skill" onChange={this.handleChange} value={this.state.skill} />
+                            <button>Add Skill</button>
+                        </form>
+                    <section/>
                     {this.renderData()}
-                
+                </div>
             </div>
+        
         );
     }
 }
